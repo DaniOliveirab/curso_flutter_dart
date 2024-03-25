19578677d649618705753df7be46c9a7d64679b7
Danielly Borges <dani.oliveiraab@gmail.com>
2024-03-25 16:09:16 -0300

    adicionando a atividade de app de jogo de dados

---
+++ jogos_dados/android/app/src/main/kotlin/com/example/jogos_dados/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jogos_dados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ jogos_dados/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal.shade700,
+        appBar: AppBar(
+          title: Text('Dadoos'),
+          backgroundColor: Colors.teal.shade900,
+        ),
+        body: Dadoos(),
+      ),
+    ),
+  );
+}
+
+class Dadoos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset('imagens/dado1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset('imagens/dado1.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
